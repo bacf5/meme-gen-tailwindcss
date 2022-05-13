322aa8db681ec8c6368e20ae5a6c4acd98104cdb
bacf5 <brunoagustincarusofassa@gmail.com>
2022-05-13 14:21:28 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/Header';
 import Meme from './components/Meme';
+import memesData from './memesData';
 
 function App() {
   return (
     <div className="text-center max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 shadow-2xl ">
       <Header />
-      <Meme />
+      <Meme item={memesData} />
     </div>
   );
 }
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,35 +1,42 @@
-export default function Meme() {
+export default function Meme({ ...props }) {
+  console.log(props);
+
+  function newMemeHandler() {
+    console.log(props);
+  }
+
   return (
     <div className="bg-white py-10 px-4  rounded-lg sm:px-10 mt-4">
-      <form>
-        <div className="flex">
-          <label
-            for="text-up"
-            class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-300"
-          ></label>
-          <input
-            type="text"
-            id="text-up"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Text up"
-          />
-          <label
-            for="text-down"
-            class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-300 m-8"
-          ></label>
-          <input
-            type="text"
-            id="text-down"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Text down"
-          />
-        </div>
-        <div className="p-12">
-          <button className="bg-gradient-to-r from-violet-600 to-fuchsia-300 h-10 pl-48 pr-48 rounded-lg font-bold text-base shadow-lg">
-            New meme 🖼
-          </button>
-        </div>
-      </form>
+      <div className="flex">
+        <label
+          for="text-up"
+          class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-300"
+        ></label>
+        <input
+          type="text"
+          id="text-up"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Text up"
+        />
+        <label
+          for="text-down"
+          class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-300 m-8"
+        ></label>
+        <input
+          type="text"
+          id="text-down"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Text down"
+        />
+      </div>
+      <div className="p-12">
+        <button
+          onClick={newMemeHandler}
+          className="bg-gradient-to-r from-violet-600 to-fuchsia-300 h-10 pl-48 pr-48 rounded-lg font-bold text-base shadow-lg"
+        >
+          New meme 🖼
+        </button>
+      </div>
     </div>
   );
 }
